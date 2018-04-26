11f4ab0890884658049b050db3caaf3bfa1c4090
lucanator420 <lucavanstraaten@icloud.com>
2018-04-26 09:24:42 +0200

    more code copyed
    just things i had in my previus code wich never made it.
    this is an unofitial branch of thad non publichd code

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-test = "hallo"
+##########_trade settings_##########
+prg_frq = 0,5 #how many minutes between checks
+#keep_profit = True #it will trade with all the funds in your wallet, else it will trade white the trade amonund.
+trade_amonund = 50 #aproxamatly the max amonund the bot will invest in one transaction at any time. schould be less than youre acount funds
+momentum = 5
+
+var1 = 6
+#what curency to check (only one)
+btc = True
+eth = False
+ltc = False
